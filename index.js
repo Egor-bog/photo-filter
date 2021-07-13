6e20e37be1f2722bff0f522e257ecf15e701cc50
Egor-bog <7548494@gmail.com>
2021-07-13 21:42:26 +0300

    fix

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const inputs = document.querySelectorAll('.filters input');
 // Обработчик кнопок
 
   //Reset
-
 document.querySelector('.btn-container .btn-reset').onclick = function() {
   inputs.forEach(function reset (input) {
     const suffix = input.dataset.sizing || '';
@@ -30,11 +29,62 @@ document.querySelector('.btn-container .btn-reset').onclick = function() {
 }
 
   //Next pictuer
+let date = new Date();
+let hours = date.getHours();
+let hoursPikcher = "";
+if (hours >=6 && hours< 12) hoursPikcher = "morning";
+if (hours >=12 && hours< 18) hoursPikcher = "day";
+if (hours >=18 && hours< 24) hoursPikcher = "evening";
+if (hours >=0 && hours< 6) hoursPikcher = "night";
 
-//https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/18.jpg
-
-//https://github.com/Egor-bog/photo-filter-image/raw/main/images/evening/01.jpg
-
+let imgUrl = "";
+let namberPikcher = 0;
 document.querySelector('.btn-container .btn-next').onclick = function () {
-  document.querySelector('#nextImg').src = "https://github.com/Egor-bog/photo-filter-image/raw/main/images/evening/01.jpg";
+  imgUrl = document.getElementById('nextImg').src;
+  console.log(imgUrl)
+  let reg = /assets/i;
+  if (reg.test(imgUrl)) {
+    namberPikcher = 1;
+    console.log(namberPikcher);
+  }
+  else {
+    ++namberPikcher;
+  }
+  if (namberPikcher > 20) {
+    namberPikcher = 1;
+  }
+  let namberPikcherNul = namberPikcher;
+  if (namberPikcherNul < 10) {
+    namberPikcherNul = ('0' + namberPikcherNul).slice(-2);}
+  console.log(namberPikcherNul)
+  document.querySelector('#nextImg').src = `https://github.com/Egor-bog/photo-filter-image/raw/main/images/${hoursPikcher}/${namberPikcherNul}.jpg`;
 }
+
+  //Load picture
+const fileInput = document.querySelector('input[type="file"]');
+fileInput.addEventListener('change', function(e) {
+  const file = fileInput.files[0];
+  const reader = new FileReader();
+  reader.onload = () => {
+    const img = new Image();
+    img.src = reader.result;
+    document.querySelector('#nextImg').src = img.src;
+  }
+  reader.readAsDataURL(file);
+});
+
+
+const canvas = document.querySelector('canvas');
+
+function drawImage() {
+  const img = new Image();  
+  img.setAttribute('crossOrigin', 'anonymous');
+  img.src = "assets/img/img.jpg";
+  img.onload = function() {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+  }; 
+}
+drawImage()
